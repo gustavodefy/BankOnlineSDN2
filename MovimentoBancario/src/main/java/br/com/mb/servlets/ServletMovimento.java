f73f4d136856f2206a69902093d5833bd7f1d804
ResVUT42 <ResVUT42@192.168.107.215>
2016-05-20 20:39:16 -0300

    RESVut42 Pedido de Venda

--- MovimentoBancario/src/main/java/br/com/mb/servlets/ServletMovimento.java
+++ MovimentoBancario/src/main/java/br/com/mb/servlets/ServletMovimento.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Servlets;
+package br.com.mb.servlets;
 
 import br.com.mb.modelo.Conta;
 import br.com.mb.modelo.Persistir;
